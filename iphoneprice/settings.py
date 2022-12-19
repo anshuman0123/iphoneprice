42feb1c1e0c241426580379128fb7d0aa5580843
Anshuman Rout <routanshuman01@gmail.com>
2022-12-19 21:27:29 +0530

    second commit

--- iphoneprice/settings.py
+++ iphoneprice/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'iphoneprice.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/ 'webapp/Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
