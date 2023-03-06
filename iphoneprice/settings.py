ee0966055a08804bd00939e3a85c69ef3a3422c2
anshuman0123 <63575217+anshuman0123@users.noreply.github.com>
2023-03-06 12:59:49 +0530

    chup mg

--- iphoneprice/settings.py
+++ iphoneprice/settings.py
@@ -123,4 +123,4 @@ MEDIA_ROOT = BASE_DIR / 'files'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS = ['https://iphoneprice-production-aafa.up.railway.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://iphoneprice-production-aafa.up.railway.app']
